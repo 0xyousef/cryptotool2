2ac7582e5fcb3a4ba843f61915e646e35be68d38
Yousef <66916030+0xyousef@users.noreply.github.com>
2022-01-30 20:04:14 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -9,10 +9,9 @@ const Layout = ({ children, title = 'Crypto Tracker' }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <header className='header'>
-        <h1>
-        
-        Heading 1 goes here 
-          </h1>
+        </br>
+        <h1>Heading 1 goes here </h1>
+</br>
          <h2> Heading 1 goes here </h2>
         <Link href='/' passHref>
           <a>
