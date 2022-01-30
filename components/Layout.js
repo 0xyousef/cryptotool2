6dad9ee0de0dde2dec481c7c332fbc041cdf1acb
Yousef <66916030+0xyousef@users.noreply.github.com>
2022-01-30 20:09:51 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -11,8 +11,8 @@ const Layout = ({ children, title = 'Crypto Tracker' }) => {
       <header className='header'>
        
         <h1>Understanding Blockchain and its correlation to Cryptocurrency</h1>
-<br>
-          <h2> (Project tool 1)<h2>
+
+          <h2><br> (Project tool 1)<h2>
         <Link href='/' passHref>
           <a>
             <svg
